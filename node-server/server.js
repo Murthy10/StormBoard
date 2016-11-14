f72264053f7b5402c894cd416a6d067efc260b4d
Murthy10 <samuel.kurath@gmail.com>
2016-11-14 15:46:43 +0100

    added server

---
+++ node-server/server.js
@@ -0,0 +1,65 @@
+var express = require('express'),
+  path = require('path'),
+  fs = require('fs');
+
+var app = express();
+var staticRoot = __dirname + '/../dist/';
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(staticRoot));
+
+app.use(function (req, res, next) {
+
+  // if the request is not html then move along
+  var accept = req.accepts('html', 'json', 'xml');
+  if (accept !== 'html') {
+    return next();
+  }
+
+  // if the request has a '.' assume that it's for a file, move along
+  var ext = path.extname(req.path);
+  if (ext !== '') {
+    return next();
+  }
+
+  fs.createReadStream(staticRoot + 'index.html').pipe(res);
+
+});
+
+//app.all('/*', function(req, res, next) {
+//    res.sendFile('index.html', { root: __dirname + '/' });
+//});
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+io.on('connection', function () { /* … */
+});
+
+
+server.listen(app.get('port'), function () {
+  console.log('app running on port', app.get('port'));
+});
+
+
+var kafka = require('kafka-node'),
+  Consumer = kafka.Consumer,
+  client = new kafka.Client('172.17.0.2:2181'),
+  consumer = new Consumer(
+    client,
+    [
+      {topic: 'results', partition: 0}
+    ],
+    {
+      autoCommit: false
+    }
+  );
+
+
+consumer.on('message', function (message) {
+  io.emit(message);
+  console.log(message);
+});
+
+
+
